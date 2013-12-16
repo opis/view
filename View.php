95ade72a5a39412809ea218a9f2f5a028d0ae7f4
msarca <marius.sarca@gmail.com>
2013-12-16 03:31:46 +0200

    Update

--- View.php
+++ View.php
@@ -1,4 +1,22 @@
 <?php
+/* ===========================================================================
+ * Opis Project
+ * http://opis.io
+ * ===========================================================================
+ * Copyright 2013 Marius Sarca
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============================================================================ */
 
 namespace Opis\View;
 
@@ -18,13 +36,8 @@ class View
     
     protected $viewKey;
     
-    public function __construct(EngineResolver $resolver = null, ViewCollection $collection = null, $insertKey = true, $viewkey = 'view')
-    {
-        if($resolver === null)
-        {
-            $resolver = new EngineResolver();
-        }
-        
+    public function __construct(EngineResolver $resolver, ViewCollection $collection = null, $insertKey = true, $viewkey = 'view')
+    {   
         if($collection === null)
         {
             $collection = new ViewCollection();
@@ -36,11 +49,6 @@ class View
         $this->viewKey = (string) $viewkey;
     }
     
-    public function registerEngineResolver($extension, Closure $resolver)
-    {
-        $this->resolver->register($extension, $resolver);
-    }
-    
     public function handle($pattern, Closure $callback, $priority = 0)
     {
         return $this->collection->add(new ViewRoute($pattern, $callback), $priority);
@@ -50,7 +58,7 @@ class View
     {
         if(!($view instanceof ViewableInterface))
         {
-            return (string) $view;
+            return $view;
         }
         $router = new ViewRouter($view->viewName(), $this->collection);
         $path = $router->execute();
