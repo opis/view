3d71c2f59c3ea02f8402a0c0a91ed47d53aebf4e
msarca <marius.sarca@gmail.com>
2016-01-15 21:09:26 +0200

    Added comments

--- lib/EngineResolver.php
+++ lib/EngineResolver.php
@@ -26,103 +26,158 @@ use Opis\Closure\SerializableClosure;
 
 class EngineResolver implements Serializable
 {
-    
+    /** @var    array */
     protected $engines = array();
-    
+
+    /** @var    \Opis\View\EngineInterface */
     protected $defaultEngine;
-    
+
+    /**
+     * Register a new engine
+     * 
+     * @param   Closure $builder
+     * @param   int     $priority
+     * 
+     * @return  \Opis\View\EngineEntry
+     */
     public function register(Closure $builder, $priority = 0)
     {
         $entry = new EngineEntry($builder);
-        
+
         $this->engines[] = array(
             'engine' => $entry,
             'priority' => $priority,
         );
-        
-        uasort($this->engines, function(&$a, &$b){
-            
-            if($a['priority'] === $b['priority'])
-            {
+
+        uasort($this->engines, function(&$a, &$b) {
+
+            if ($a['priority'] === $b['priority']) {
                 return 0;
             }
-            
+
             return $a['priority'] < $b['priority'] ? 1 : -1;
         });
-        
+
         return $entry;
     }
-    
+
+    /**
+     * Resolve a path to a render engine
+     * 
+     * @param   string  $path
+     * 
+     * @return  \Opis\View\EngineInterface
+     */
     public function resolve($path)
     {
-        foreach($this->engines as &$entry)
-        {
+        foreach ($this->engines as &$entry) {
             $engine = $entry['engine'];
-            
-            if($engine->canHandle($path))
-            {
+
+            if ($engine->canHandle($path)) {
                 return $engine->instance();
             }
         }
         return $this->getDefaultEngine();
     }
-    
+
+    /**
+     * Get the default render engine
+     * 
+     * @return \Opis\View\EngineInterface
+     */
     protected function getDefaultEngine()
     {
-        if($this->defaultEngine === null)
-        {
+        if ($this->defaultEngine === null) {
             $this->defaultEngine = new PHPEngine();
         }
         return $this->defaultEngine;
     }
-    
+
+    /**
+     * Serialize
+     * 
+     * @return  string
+     */
     public function serialize()
     {
         return serialize($this->engines);
     }
-    
+
+    /**
+     * Unserialize
+     * 
+     * @param   string  $data
+     */
     public function unserialize($data)
     {
         $this->engines = unserialize($data);
     }
-    
 }
 
 class EngineEntry implements Serializable
 {
+    /** @var    Closure */
     protected $builder;
-    
+
+    /** @var    Closure */
     protected $handler;
-    
+
+    /** @var    \Opis\View\EngineInterface */
     protected $instance;
-    
+
+    /**
+     * Constructor
+     * 
+     * @param   Closure $builder
+     */
     public function __construct(Closure $builder)
     {
         $this->builder = $builder;
     }
-    
+
+    /**
+     * Add a handler callback
+     * 
+     * @param   Closure $handler
+     */
     public function handle(Closure $handler)
     {
         $this->handler = $handler;
     }
-    
+
+    /**
+     * Check if the path can be handled
+     * 
+     * @param   string  $path
+     * 
+     * @return  boolean
+     */
     public function canHandle($path)
     {
         $handler = $this->handler;
         return $handler($path);
     }
-    
+
+    /**
+     * Get an instance of an engine
+     * 
+     * @return  \Opis\View\EngineInterface
+     */
     public function instance()
     {
-        if($this->instance === null)
-        {
+        if ($this->instance === null) {
             $builder = $this->builder;
-            $this->instance = $builder();   
+            $this->instance = $builder();
         }
-        
+
         return $this->instance;
     }
-    
+
+    /**
+     * Serialize
+     * 
+     * @return  string
+     */
     public function serialize()
     {
         SerializableClosure::enterContext();
@@ -133,7 +188,12 @@ class EngineEntry implements Serializable
         SerializableClosure::exitContext();
         return $object;
     }
-    
+
+    /**
+     * Unserialize
+     * 
+     * @param   string  $data
+     */
     public function unserialize($data)
     {
         $object = SerializableClosure::unserializeData($data);
